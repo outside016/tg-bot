ebea8e9680135712d408a857cca16f1eb0609449
outside016 <outside016@icloud.com>
2024-01-14 00:49:24 +0300

    make products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,18 +5,17 @@ import {IProduct} from "../services/products.service";
   selector: 'app-product-list',
   standalone: true,
   template: `
-      <h2>{{ title }}</h2>
+      <h1>{{ title }}</h1>
       <ul class="products">
           @for (product of products; track product.id) {
               <li class="product-item">
                   <div class="product-image">
-                      <img [src]="product.image" [alt]="product.name"/>
+                      <img class="product-img" [src]="product.image" [alt]="product.name"/>
                   </div>
                   <div class="product-info">
                       <h3>{{ product.brand }}</h3>
                       <p class="hint">{{product.name}}</p>
-                      <p class="hint">{{product.type}}</p>
-                      <p class="hint">{{product.price | }}</p>
+                      <h2 class="hint">{{product.price}} р.</h2>
                   </div>
               </li>
           }
