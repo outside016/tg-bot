eb690de81b73f60d80e19a816f0b51943ba10eba
outside016 <outside016@icloud.com>
2024-01-15 18:43:24 +0300

    make products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,14 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {IProduct} from "../services/products.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
+  imports: [RouterLink],
   template: `
       <h1>{{ title }}</h1>
       <ul class="products">
           @for (product of products; track product.id) {
-              <li class="product-item">
+              <li class="product-item" [routerLink]="'product/' + product.id">
                   <div class="product-image">
                       <img class="product-img" [src]="product.image" [alt]="product.name"/>
                   </div>
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IProduct, ProductsService} from "../services/products.service";
+import {TelegramService} from "../services/telegram.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
-  template: `<h2>product</h2>`
+  template: `
+      <div class="centered">
+          <h2 class="mb">{{ product.brand }}</h2>
+          <h2 class="mb">{{ product.name }}</h2>
+          <br/>
+          <img class="product-img" [src]="product.image" [alt]="product.name">
+          <br/>
+          <h2>{{ product.price }}р.</h2>
+          <br/>
+          <a [href]="product.link" target="_blank">Посмотреть на сайте NUW.STORE</a>
+      </div>
+  `
 })
 export class ProductComponent {
 
+  product: IProduct;
+
+  constructor(
+    private products: ProductsService,
+    private telegram: TelegramService,
+    private route: ActivatedRoute,
+    private Router: Router) {
+
+    const id = this.route.snapshot.paramMap.get('id');
+    this.product = this.products.getById(id);
+
+  }
 }
